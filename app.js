d2c6bdb6a028a31be9e566436b413094f8ab98d9
Franky Hung <arkon.digital.hcf@gmail.com>
2026-04-01 14:57:13 +0800

    update the endpoints to start at /webhook

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
-app.get("/", (req, res) => {
+app.get("/webhook", (req, res) => {
     const {
         "hub.mode": mode,
         "hub.challenge": challenge,
@@ -28,11 +28,16 @@ app.get("/", (req, res) => {
 });
 
 // Route for POST requests
-app.post("/", (req, res) => {
+app.post("/webhook", (req, res) => {
     const timestamp = new Date().toISOString().replace("T", " ").slice(0, 19);
     console.log(`\n\nWebhook received ${timestamp}\n`);
     console.log(JSON.stringify(req.body, null, 2));
     res.status(200).end();
+});
+
+app.get("/", (req, res) => {
+    res.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`);
 });
 
 // Start the server
